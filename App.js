b12a69ddb2afc56716508c3ac1fc1cfe453a133a
Hani-ch-24 <chhanou8@gmail.com>
2025-06-09 21:22:55 +0100

    Create App.js

---
+++ App.js
@@ -0,0 +1,17 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header>
+        <h1>Welcome to My First React Website!</h1>
+      </header>
+      <main>
+        <p>This is hosted on GitHub Pages. 🚀</p>
+        <a href="https://github.com/yourusername">Visit my GitHub</a>
+      </main>
+    </div>
+  );
+}
+
+export default App;
